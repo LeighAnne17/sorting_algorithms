c62607d7fb84e21ea2c0e15474bc428abaa72159
LeighAnne17 <leighndimande17@icloud.com>
2024-05-04 05:58:38 -0700

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,14 +12,15 @@ void bubble_sort(int *array, size_t size)
 {
     size_t i, j;
     int temp;
-    int swapped;
 
-    for (i = 0; i < size - 1; i++)
+    if (size < 2)
+	    return;
+
+    for (i = 0; i < size; i++)
     {
-        swapped = 0;
-        for (j = 0; j < size - i - 1; j++)
+        for (j = 0; j < size; j++)
         {
-            if (array[j] > array[j + 1])
+            if (array[j] > array[j + 1] && array[j + 1])
             {
                 temp = array[j];
                 array[j] = array[j + 1];
@@ -28,8 +29,5 @@ void bubble_sort(int *array, size_t size)
                 print_array(array, size);
             }
         }
-
-        if (!swapped)
-            break;
     }
 }
