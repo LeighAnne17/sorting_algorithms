703261f9d7dd2fea9483409aabef2ba644db8a8e
LeighAnne17 <leighndimande17@icloud.com>
2024-05-04 06:31:58 -0700

    sorting algorithms

--- sort.h
+++ sort.h
@@ -1,6 +1,6 @@
 #ifndef SORT_H
 #define SORT_H
-
+#include <stddef.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -29,6 +29,5 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
 
 #endif
